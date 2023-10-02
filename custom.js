b0cea4129acb40dcb1c5f7ac064c5a184aea76ef
addictedbot1 <107464022+addictedbot1@users.noreply.github.com>
2023-10-02 12:10:11 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,2 +1,7 @@
-var myElement = document.getElementById('simple-bar');
-new SimpleBar(myElement, { autoHide: true });
+// var myElement = document.getElementById('simple-bar');
+// new SimpleBar(myElement, { autoHide: true });
+
+setTimeout(function(){
+  var myElement = document.getElementById('simple-bar');
+  new SimpleBar(myElement, { autoHide: true });
+}, 10000);
